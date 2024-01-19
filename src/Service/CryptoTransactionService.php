3d370542f8f1e030144d14eef5faaf9c27e191b2
harshana disanayaka <karudisa26@gmail.com>
2024-01-19 01:38:21 +0100

    fix error handling

--- src/Service/CryptoTransactionService.php
+++ src/Service/CryptoTransactionService.php
@@ -5,29 +5,45 @@ use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use App\Entity\Transaction;
 use App\Service\CryptoAnalyseService;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
+
+
+
 
 class CryptoTransactionService
 {
     private $cryptoAnalyseService;
     private $entityManager;
-    private $managerRegistry;
+    private $logger;
 
-    public function __construct(CryptoAnalyseService $cryptoAnalyseService, EntityManagerInterface $entityManager, ManagerRegistry $managerRegistry)
+    public function __construct(CryptoAnalyseService $cryptoAnalyseService, EntityManagerInterface $entityManager, LoggerInterface $logger)
     {
         $this->cryptoAnalyseService = $cryptoAnalyseService;
         $this->entityManager = $entityManager;
-        $this->managerRegistry = $managerRegistry;
+        $this->logger = $logger;
     }
 
     public function analyzeAndProcessTransactions($network, $asset, $version, $address)
     {
-        //$apiContext = $this->cryptoAnalyseService->createApiContext($network, $asset, $version);
         $txs = $this->cryptoAnalyseService->getTransactionsForAddress($network, $asset, $version, $address);
 
         foreach ($txs as $tx) {
             $confirmedDate = $tx->confirmed;
-
+    
+            // Check if the transaction already exists in the database
+            $existingTransaction = $this->entityManager->getRepository(Transaction::class)->findOneBy([
+                'address' => $address,
+                'asset' => $asset,
+                'network' => $network,
+                'version' => $version,
+                'confirmedDate' => new \DateTime($confirmedDate),
+            ]);
+    
+            if ($existingTransaction) {
+                // If the transaction already exists, skip to the next iteration
+                continue;
+            }
+    
             try {
                 $transaction = new Transaction();
                 $transaction->setAddress($address);
@@ -35,18 +51,12 @@ class CryptoTransactionService
                 $transaction->setNetwork($network);
                 $transaction->setVersion($version);
                 $transaction->setConfirmedDate(new \DateTime($confirmedDate));
-
-                $this->entityManager->getRepository(Transaction::class);
+    
                 $this->entityManager->persist($transaction);
                 $this->entityManager->flush();
             } catch (UniqueConstraintViolationException $e) {
-                if (!$this->entityManager->isOpen()) {
-                    $this->entityManager = $this->managerRegistry->resetManager();
-                }
+               $this->logger->error(''. $e->getMessage());
             }
-
-
-
         }
 
         return $txs;
